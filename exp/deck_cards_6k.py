0aac8ae9324ab2db04a7f36a04459afa6eac8a3a
Sanefoxx <alexndr.lisitsin@gmail.com>
2018-04-17 13:42:53 +0300

    deck of cards

---
+++ exp/deck_cards_6k.py
@@ -0,0 +1,2 @@
+def deck_of_cards(): return [(x + y) for x in ["ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"] for y in [" of hearts", " of spades", " of diamonds", " of clubs"]]
+
